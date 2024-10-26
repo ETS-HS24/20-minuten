8b3ee6920f35ec020495fe2e746e9cddd4be3ca5
Danielgergely <daniel.gergely@uzh.ch>
2024-10-26 14:38:09 +0200

    article content pre-processing integrated into pipeline

--- main.py
+++ main.py
@@ -3,23 +3,31 @@ import os
 from pipeline.service import FileService, SentimentService, TextService
 
 if __name__ == "__main__":
+
+    ######## Read FIle #########
+
     search_pattern = os.path.join('data', "**", "raw-data", "*.tsv")
 
     mathing_files = glob.glob(search_pattern, recursive=True)
     file_path = mathing_files[0]
 
-    # Read the TSV file into a DataFrame
     articles_df = FileService.read_tsv_to_df(file_path)
-    # Save the DataFrame as a parquet file
     FileService.df_to_parquet(articles_df, 'articles_raw')
 
-    # Remove tags from the content column
-    cleaned_articles_df = TextService.remove_tags(articles_df)
-    # Add sentiment to each article
-    sentiment_df = SentimentService.sentimental_analysis(cleaned_articles_df)
-    # Save the DataFrame as a parquet file
-    FileService.df_to_parquet(sentiment_df, 'articles_sentiment')
+    ######### Process Data #########
+    articles_df = TextService.dop_columns(df=articles_df, columns_to_drop=["rubric", "regional", "subhead"])
+    articles_df = TextService.process_tags(df=articles_df)
 
-    sentiment_df = FileService.read_parquet_to_df('articles_sentiment')
+
+    # # Remove tags from the content column
+    # cleaned_articles_df = TextService.remove_all_tags(articles_df)
+    # # Add sentiment to each article
+    # sentiment_df = SentimentService.sentimental_analysis(cleaned_articles_df)
+    # # Save the DataFrame as a parquet file
+    # FileService.df_to_parquet(sentiment_df, 'articles_sentiment')
+    #
+    # sentiment_df = FileService.read_parquet_to_df('articles_sentiment')
+    #
+    # a = FileService.read_parquet_to_df('articles_sentiment')
     a=10
 
--- pipeline/service/_text.py
+++ pipeline/service/_text.py
@@ -4,9 +4,40 @@ import pandas as pd
 class TextService:
 
     @staticmethod
-    def remove_tags(df: pd.DataFrame, columns_to_clean=None) -> pd.DataFrame:
+    def dop_columns(df: pd.DataFrame, columns_to_drop: list) -> pd.DataFrame:
+        return df.drop(columns_to_drop, axis=1)
+
+
+    @staticmethod
+    def remove_all_tags(df: pd.DataFrame, columns_to_clean=None) -> pd.DataFrame:
         if columns_to_clean is None:
             columns_to_clean = ["content"]
         for column in columns_to_clean:
             df[column] = df[column].str.replace(r'<[^>]*>', '', regex=True)
+        return df
+
+    @staticmethod
+    def process_tags(df: pd.DataFrame) -> pd.DataFrame:
+        # Extract and remove lead text
+        df['lead_text'] = df['content'].str.extract(r'<ld>(.*?)</ld>', expand=False)
+        df['content'] = df['content'].str.replace(r'<ld>.*?</ld>', '', regex=True)
+
+        # Extract and remove subheadings
+        df['subheadings'] = df['content'].str.extract(r'<zt>(.*?)</zt>', expand=False)
+        df['content'] = df['content'].str.replace(r'<zt>.*?</zt>', '', regex=True)
+
+        # Extract and remove author full name
+        df['author'] = df['content'].str.extract(r'<au>(.*?)</au>', expand=False)
+        df['content'] = df['content'].str.replace(r'<au>.*?</au>', '', regex=True)
+
+        # Remove tags but keep annotated text within <a> tags
+        df['content'] = df['content'].str.replace(r'<a[^>]*>(.*?)</a>', r'\1', regex=True)
+
+        # Remove <tx>, <p>, <br>, <ka>, and <lg> tags
+        df['content'] = df['content'].str.replace(r'</?(tx|p|br|ka|lg)[^>]*>', '', regex=True)
+
+        # Remove last <p> element if it matches authors and extract to column
+        df['author_extracted'] = df['content'].str.extract(r'<p>(.*?)(?=</p>$)', expand=False)
+        df['content'] = df['content'].str.replace(r'<p>.*?</p>$', '', regex=True)
+
         return df
