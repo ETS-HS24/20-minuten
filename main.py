bb88c83d993021bc042442132a0cf3e7b99535ad
Danielgergely <daniel.gergely@uzh.ch>
2024-11-08 01:15:27 +0100

    reverted some changes and added column to process as an optional parameter
    to both lemmatize content and sentimental analysis

--- main.py
+++ main.py
@@ -21,12 +21,11 @@ if __name__ == "__main__":
     matching_files = glob.glob(search_pattern, recursive=True)
     file_path = matching_files[0]
 
-
     if (
-        not Path(FileService.get_parquet_path(file_name='articles_raw')).exists() 
-        or force_recreate
-        or _previous_step_recreate
-        ): 
+            not Path(FileService.get_parquet_path(file_name='articles_raw')).exists()
+            or force_recreate
+            or _previous_step_recreate
+    ):
         logger.info(f"Recreating raw parquet.")
         articles_df = FileService.read_tsv_to_df(file_path)
         FileService.df_to_parquet(df=articles_df, file_name='articles_raw')
@@ -36,45 +35,46 @@ if __name__ == "__main__":
         articles_df = FileService.read_parquet_to_df(file_name='articles_raw')
 
     if (
-        not Path(FileService.get_parquet_path(file_name='articles_cleaned')).exists() 
-        or force_recreate
-        or _previous_step_recreate
-        ): 
+            not Path(FileService.get_parquet_path(file_name='articles_cleaned')).exists()
+            or force_recreate
+            or _previous_step_recreate
+    ):
         logger.info("Recreating cleaned dataset.")
         ######### Pre-process Data #########
-        cleaned_articles_df = TextService.drop_columns(df=articles_df, columns_to_drop=["rubric", "regional", "subhead"])
+        cleaned_articles_df = TextService.drop_columns(df=articles_df,
+                                                       columns_to_drop=["rubric", "regional", "subhead"])
         cleaned_articles_df = TextService.process_tags(df=cleaned_articles_df)
         FileService.df_to_parquet(cleaned_articles_df, 'articles_cleaned')
         _previous_step_recreate = True
-    
+
     else:
         logger.info("Not recreating cleaned dataset.")
         cleaned_articles_df = FileService.read_parquet_to_df(file_name='articles_cleaned')
 
-    
     if (
-        not Path(FileService.get_parquet_path(file_name='articles_lemmatized_spacy')).exists()
-        or force_recreate
-        or _previous_step_recreate
-        ):
+            not Path(FileService.get_parquet_path(file_name='articles_lemmatized')).exists()
+            or force_recreate
+            or _previous_step_recreate
+    ):
         logger.info("Recreating lemmatized dataset.")
         ########## Lemmatize Data #########
-        lemmatized_articles_df = TextService.lemmatize_content_spacy(df=cleaned_articles_df)
-        FileService.df_to_parquet(lemmatized_articles_df, 'articles_lemmatized_spacy')
+        lemmatized_articles_df = TextService.lemmatize_content_nltk(df=cleaned_articles_df)
+        FileService.df_to_parquet(lemmatized_articles_df, 'articles_lemmatized')
         _previous_step_recreate = True
     else:
         logger.info("Not recreating lemmatized dataset.")
         lemmatized_articles_df = FileService.read_parquet_to_df(file_name='articles_lemmatized')
 
     if (
-        not Path(FileService.get_parquet_path(file_name='articles_sentiment')).exists() 
-        or force_recreate
-        or _previous_step_recreate
-        ): 
+            not Path(FileService.get_parquet_path(file_name='articles_sentiment')).exists()
+            or force_recreate
+            or _previous_step_recreate
+    ):
         logger.info(f"Recreating sentiment analysis")
         ############# Sentiment Analysis ############
         # Add sentiment to each article
-        sentiment_df = SentimentService.sentimental_analysis(cleaned_articles_df)
+        sentiment_df = SentimentService.sentimental_analysis(data_frame=lemmatized_articles_df,
+                                                             column_to_process="content_lemmatized")
         # Save the DataFrame as a parquet file
         FileService.df_to_parquet(sentiment_df, 'articles_sentiment')
         _previous_step_recreate = True
@@ -90,8 +90,9 @@ if __name__ == "__main__":
 
     # French
     french_series = sentiment_df[sentiment_df['language'] == 'fr'].iloc[:number_of_articles]['content']
-    #french_series = sentiment_df[sentiment_df['language'] == 'fr']['content']
-    french_model, _, _ = TopicModellingService.fit_lda(texts=french_series, language="french", num_topics=number_of_topics, dataset_passes=ds_passes)
+    # french_series = sentiment_df[sentiment_df['language'] == 'fr']['content']
+    french_model, _, _ = TopicModellingService.fit_lda(texts=french_series, language="french",
+                                                       num_topics=number_of_topics, dataset_passes=ds_passes)
     french_top_words_per_topic = TopicModellingService.lda_top_words_per_topic(model=french_model, n_top_words=10)
     model_path = TopicModellingService.save_gensim_model(model=french_model, language="french")
     model1 = TopicModellingService.load_gensim_model(language="french")
@@ -102,19 +103,20 @@ if __name__ == "__main__":
     df_topics_fr = french_top_words_per_topic
 
     # German
-    #german_series = sentiment_df[sentiment_df['language'] == 'de'].iloc[:number_of_articles]['content']
+    # german_series = sentiment_df[sentiment_df['language'] == 'de'].iloc[:number_of_articles]['content']
     german_series = sentiment_df[sentiment_df['language'] == 'de']['content']
-    german_model, _, _ = TopicModellingService.fit_lda(texts=german_series, language="german", num_topics=number_of_topics, dataset_passes=ds_passes)
+    german_model, _, _ = TopicModellingService.fit_lda(texts=german_series, language="german",
+                                                       num_topics=number_of_topics, dataset_passes=ds_passes)
     german_top_words_per_topic = TopicModellingService.lda_top_words_per_topic(model=german_model, n_top_words=10)
     model_path = TopicModellingService.save_gensim_model(model=german_model, language="german")
 
     FileService.df_to_csv(df=german_top_words_per_topic, file_name="topics_de")
 
-
     df_topics_de = german_top_words_per_topic
 
     # Matching topics German / French
-    hit_list, corpus_embedding, top_k = TopicMatcherService.match(df_topics_de['Word'], df_topics_fr['Word'], print_matches=False)
+    hit_list, corpus_embedding, top_k = TopicMatcherService.match(df_topics_de['Word'], df_topics_fr['Word'],
+                                                                  print_matches=False)
 
     # print(hit_list)
     # print(corpus_embedding)
--- pipeline/service/_text.py
+++ pipeline/service/_text.py
@@ -15,26 +15,6 @@ from nltk.data import find
 
 
 class TextService:
-    nlp_de = spacy.load("de_core_news_sm")
-    nlp_fr = spacy.load("fr_core_news_sm")
-    nltk_data_downloaded = False
-
-    @staticmethod
-    def download_nltk_data():
-        if not TextService.nltk_data_downloaded:
-            try:
-                find('corpora/stopwords.zip')
-            except LookupError:
-                import nltk
-                nltk.download('stopwords')
-
-            try:
-                find('corpora/wordnet.zip')
-            except LookupError:
-                import nltk
-                nltk.download('wordnet')
-
-            TextService.nltk_data_downloaded = True
 
     @staticmethod
     def drop_columns(df: pd.DataFrame, columns_to_drop: list) -> pd.DataFrame:
@@ -127,8 +107,9 @@ class TextService:
         return df
 
     @staticmethod
-    def lemmatize_content_nltk(df: pd.DataFrame) -> pd.DataFrame:
-        TextService.download_nltk_data()
+    def lemmatize_content_nltk(df: pd.DataFrame, column_to_process: str = "content") -> pd.DataFrame:
+        nlp_de = spacy.load("de_core_news_sm")
+        nlp_fr = spacy.load("fr_core_news_sm")
         lemmatizer = WordNetLemmatizer()
 
         custom_stop_words: set = {
@@ -146,13 +127,13 @@ class TextService:
 
         def lemmatize_text(doc: str, lang: str):
             if lang == "fr":
-                doc = TextService.nlp_fr(str(doc).lower())
+                doc = nlp_fr(str(doc).lower())
                 tokenized_article = [token.text for token in doc if not token.is_stop and not token.is_punct]
                 lemmatized_article = [lemmatizer.lemmatize(token) for token in tokenized_article if
                                       token not in french_stop_words]
 
             else:
-                doc = TextService.nlp_de(str(doc).lower())
+                doc = nlp_de(str(doc).lower())
                 tokenized_article = [token.text for token in doc if not token.is_stop and not token.is_punct]
                 lemmatized_article = [lemmatizer.lemmatize(token) for token in tokenized_article if
                                       token not in german_stop_words]
@@ -161,18 +142,20 @@ class TextService:
 
         tqdm.pandas()
         df["content_lemmatized"] = df.progress_apply(
-            lambda x: pd.Series(lemmatize_text(x["content"], x["language"])), axis=1
+            lambda x: pd.Series(lemmatize_text(x[column_to_process], x["language"])), axis=1
         )
         return df
 
     @staticmethod
     def lemmatize_content_spacy(df: pd.DataFrame) -> pd.DataFrame:
+        nlp_de = spacy.load("de_core_news_sm")
+        nlp_fr = spacy.load("fr_core_news_sm")
 
         def lemmatize_text(doc: str, lang: str):
             if lang == "fr":
-                docs = TextService.nlp_fr.pipe([doc], disable=["tagger", "ner", "textcat"], n_process=4)
+                docs = nlp_fr.pipe([doc], disable=["tagger", "ner", "textcat"], n_process=4)
             else:
-                docs = TextService.nlp_de.pipe([doc], disable=["tagger", "ner", "textcat"], n_process=4)
+                docs = nlp_de.pipe([doc], disable=["tagger", "ner", "textcat"], n_process=4)
 
             alphas = [token.lemma_.lower() for doc in docs for token in doc if
                       not token.is_alpha and not token.is_punct and not token.is_space]
